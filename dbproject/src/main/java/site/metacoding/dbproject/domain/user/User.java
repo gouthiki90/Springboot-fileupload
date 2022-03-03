6ff6966630a263a606b8f77d9f28525ff145a946
전가은 <jeonga-eun@jeonga-eun-ui-MacBook-Air.local>
2022-03-03 16:59:05 +0900

    웹페이지 UI 만들기

--- dbproject/src/main/java/site/metacoding/dbproject/domain/user/User.java
+++ dbproject/src/main/java/site/metacoding/dbproject/domain/user/User.java
@@ -9,11 +9,18 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 // JPA 라이브러리는 Java Persistence(영구적인) API(노출되어 있는 메서드)이라고 한다. 
 // 1. CRUD 메서드 기본 제공
 // 2. 자바코드로 DB를 자동 생성하는 기능 제공 - 설정파일 설정하기
-// 3. ORM 제공
+// 3. ORM 제공 - Object Realtion Mapping
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 @Entity // 서버 실행시 해당 클래스로 테이블을 생성
 public class User {
     // IDENTITY 전략은 DB에게 번호증가 전략을 위임하는 것 - 알아서 디비에 맞게 찾아준다.
@@ -28,7 +35,6 @@ public class User {
 
     @Column(length = 16000000)
     private String email;
-    private String phone;
 
     private LocalDateTime createDate;
 }
